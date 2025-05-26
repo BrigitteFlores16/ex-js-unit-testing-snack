b15596f3a9767397dfbf7f7b8d838ceb1329967f
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-05-26 12:53:39 +0200

    first commit

---
+++ test-snack.test.js
@@ -0,0 +1,31 @@
+//snack 1
+//Creare un test che verifichi la seguente descrizione:
+//"La funzione getInitials restituisce le iniziali di un nome completo."
+
+//snack 2
+//Creare un test che verifichi la seguente descrizione:
+// "La funzione createSlug restituisce una stringa in lowercase."
+
+// snack 3
+// Creare un test che verifichi la seguente descrizione:
+// "La funzione average calcola la media aritmetica di un array di numeri."
+
+// snack 4
+// Creare un test che verifichi la seguente descrizione:
+//"La funzione createSlug sostituisce gli spazi con -."
+//Esempi:createSlug("Questo è un test") → "questo-e-un-test"
+
+// snack 5
+// Creare un test che verifichi la seguente descrizione:
+//"La funzione isPalindrome verifica se una stringa è un palindromo."
+//Nota: una stringa palindroma è una sequenza di caratteri che si legge uguale sia da sinistra a destra che da destra a sinistra.
+
+// snack 6
+// Creare un test che verifichi la seguente descrizione:
+// "La funzione isAnagram verifica se due stringhe sono anagrammi l'una dell'altra."
+
+// snack 7
+// Crea un array di oggetti posts, in cui ogni oggetto ha le proprietà id, title e slug.
+// Creare un test che verifichi le seguenti descrizioni:
+// "La funzione findPostById restituisce il post corretto dato l’array di post e l’id"
+//Creare uno o più test aggiuntivi che controllino che la struttura dati passati sia conforme (ogni post ha le proprietà id, title e slug, viene passato un id numerico).
